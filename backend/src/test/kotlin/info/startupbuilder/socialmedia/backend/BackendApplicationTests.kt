a8967eeaef45d7bad83df256ae7d69f6fec51a6a
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-11 10:26:46 +0800

    Generate spring backend from start.spring.io

---
+++ backend/src/test/kotlin/info/startupbuilder/socialmedia/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.startupbuilder.socialmedia.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
